aa99ca92d367db364590ee62c503d115a2ae17dd
unknown <surojdyola@gmail.com>
2019-12-10 14:49:51 +0545

    first commit

--- toasterlibrary/src/main/java/com/cellpay/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/cellpay/toasterlibrary/ToasterMessage.java
@@ -1,11 +1,14 @@
 package com.cellpay.toasterlibrary;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 public class ToasterMessage {
     public static void s(Context c, String message){
-        Toast.makeText(c,message,Toast.LENGTH_SHORT);
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+        Log.d("Hello","K bhAYO ");
 
     }
 }
